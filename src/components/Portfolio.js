6f9644e0f35fdb7676378c55c700d234b2c30987
Abhishek Mandloi <mandloi.abhishek9451@gmail.com>
2025-07-21 18:31:14 +0530

    Update Portfolio component, package-lock, and add new images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,12 +12,30 @@ const Portfolio = () => {
       image: `${process.env.PUBLIC_URL}/podcastmonkey.png`, // Full-page screenshot
     },
     {
+      title: "PurPet",
+      description: "​Pür Pet creates eco-friendly, vet-approved pet products focused on comfort, style, wellness, and orthopedic support for modern pets.",
+      link: "https://purpetco.com/",
+      image: `${process.env.PUBLIC_URL}/pur-pet.png`,
+    },
+    {
+      title: "Elite honey",
+      description: "​Pür Pet creates eco-friendly, vet-approved pet products focused on comfort, style, wellness, and orthopedic support for modern pets.",
+      link: "https://elitehoneysupplements.com/",
+      image: `${process.env.PUBLIC_URL}/elite-honey.png`,
+    },
+     {
       title: "Raan Cotton",
       description: "​RAAN offers eco-friendly, unbleached cotton wipes—plastic-free, biodegradable, and gentle for all ages.​",
       link: "https://raan.com/",
       image: `${process.env.PUBLIC_URL}/raan-cotton.png`,
     },
-    {       
+    {
+      title: "Kick It",
+      description: "Kickit offers a fun and easy-to-play soccer tennis game, perfect for all ages and skill levels.",
+      link: "https://kickit.net/",
+      image: `${process.env.PUBLIC_URL}/kick-it.png`,
+    },
+    {
       title: "DesMirage",
       description: "Desmirage offers high-quality perfumes inspired by luxury fragrances, crafted with organic ingredients and eco-friendly packaging .​",
       link: "https://www.desmirage.com/",
@@ -29,36 +47,30 @@ const Portfolio = () => {
       link: "https://www.appalife.com/",
       image: `${process.env.PUBLIC_URL}/appa-life.png`,
     },
-    {
-      title: "Blow Gas",
-      description: "Blow Gas Co sells a variety of hemp products, including THCA flowers, vapes, and edibles.",
-      link: "https://blowgas.com/",
-      image: `${process.env.PUBLIC_URL}/blow-gas.png`,
-    },
+    // {
+    //   title: "Blow Gas",
+    //   description: "Blow Gas Co sells a variety of hemp products, including THCA flowers, vapes, and edibles.",
+    //   link: "https://blowgas.com/",
+    //   image: `${process.env.PUBLIC_URL}/blow-gas.png`,
+    // },
     {
       title: "Lammar Marie",
       description: "Lammar Marie offers gourmet popcorn with unique flavors, perfect for gifting or special occasions.",
       link: "https://lammarmarie.com/",
       image: `${process.env.PUBLIC_URL}/lammarmarie.png`,
     },
-    {
-      title: "Clean Pets",
-      description: "Clean Pets offers natural wellness chews for pets, supporting gut health, joints, skin, and more.",
-      link: "https://shopcleanpets.com/",
-      image: `${process.env.PUBLIC_URL}/clean-pets.png`,
-    },
-    {
-      title: "Kick It",
-      description: "Kickit offers a fun and easy-to-play soccer tennis game, perfect for all ages and skill levels.",
-      link: "https://kickit.net/",
-      image: `${process.env.PUBLIC_URL}/kick-it.png`,
-    },
-    {
-      title: "Native Ceuticals",
-      description: "PureSkin Organic offers natural, organic skincare products for rejuvenation, protection, and nourishment without chemicals.",
-      link: "https://pureskinorganic.com/",
-      image: `${process.env.PUBLIC_URL}/pureskinorganic.png`,
-    },
+    // {
+    //   title: "Clean Pets",
+    //   description: "Clean Pets offers natural wellness chews for pets, supporting gut health, joints, skin, and more.",
+    //   link: "https://shopcleanpets.com/",
+    //   image: `${process.env.PUBLIC_URL}/clean-pets.png`,
+    // },
+    // {
+    //   title: "Native Ceuticals",
+    //   description: "PureSkin Organic offers natural, organic skincare products for rejuvenation, protection, and nourishment without chemicals.",
+    //   link: "https://pureskinorganic.com/",
+    //   image: `${process.env.PUBLIC_URL}/pureskinorganic.png`,
+    // },
     {
       title: "Drink Miils",
       description: "​MIILS offers space-inspired meal shakes with 19g protein, 3g sugar, and crispy inclusions. ",
@@ -90,7 +102,7 @@ const Portfolio = () => {
             </a>
 
             {/* Scrolling Image */}
-            <div 
+            <div
               className="scroll-container"
               onClick={() => setSelectedImage(project.image)}
             >
@@ -103,24 +115,25 @@ const Portfolio = () => {
         <h3>🔗 More...</h3>
         <div className="all-link">
 
-        <a className="link-website" href="https://reviglow.com/" target="_blank" rel="noopener noreferrer">Revi Glow</a>
-        <a className="link-website" href="https://hookyaproducts.com/" target="_blank" rel="noopener noreferrer">Hook Ya</a>
-        <a className="link-website" href="https://strongrrsupplements.com/" target="_blank" rel="noopener noreferrer">Strongrr</a>
-        <a className="link-website" href="https://naturefusionglow.com/" target="_blank" rel="noopener noreferrer">Nature Fusion Glow</a>
-        <a className="link-website" href="https://everwellhemp.com/" target="_blank" rel="noopener noreferrer">Everwell Hamp</a>
-        <a className="link-website" href="https://getmindright.com/" target="_blank" rel="noopener noreferrer">Mind Right</a>
-        <a className="link-website" href="https://greenvirginproducts.com/" target="_blank" rel="noopener noreferrer">Green Virgin</a>
+          <a className="link-website" href="https://reviglow.com/" target="_blank" rel="noopener noreferrer">Revi Glow</a>
+          <a className="link-website" href="https://hookyaproducts.com/" target="_blank" rel="noopener noreferrer">Hook Ya</a>
+          <a className="link-website" href="https://strongrrsupplements.com/" target="_blank" rel="noopener noreferrer">Strongrr</a>
+          <a className="link-website" href="https://naturefusionglow.com/" target="_blank" rel="noopener noreferrer">Nature Fusion Glow</a>
+          <a className="link-website" href="https://everwellhemp.com/" target="_blank" rel="noopener noreferrer">Everwell Hamp</a>
+          <a className="link-website" href="https://getmindright.com/" target="_blank" rel="noopener noreferrer">Mind Right</a>
+          <a className="link-website" href="https://greenvirginproducts.com/" target="_blank" rel="noopener noreferrer">Green Virgin</a>
 
-        <a className="link-website" href="https://www.desmirage.com/" target="_blank" rel="noopener noreferrer">DesMirage</a>
-        <a className="link-website" href="https://www.casesbysource.com/" target="_blank" rel="noopener noreferrer">Cases By Source</a>
-        <a className="link-website" href="https://thepowerfit.com/" target="_blank" rel="noopener noreferrer">Power Fit </a>
-        <a className="link-website" href="https://runur.run/" target="_blank" rel="noopener noreferrer">Runer Run</a>
-        <a className="link-website" href="https://bddbrands.com/" target="_blank" rel="noopener noreferrer">BDD Brands </a>
-        <a className="link-website" href="https://www.colorlabtransfers.com/" target="_blank" rel="noopener noreferrer">ColorLab Transfer</a>
-        <a className="link-website" href="https://ipsopromo.com/pages/landing-page" target="_blank" rel="noopener noreferrer">Ipso Facto</a>
-        <a className="link-website" href="https://purpetco.com/" target="_blank" rel="noopener noreferrer">PurPet </a>
-        <a className="link-website" href="https://healthvape.com/" target="_blank" rel="noopener noreferrer">Health vape</a>
-        <a className="link-website" href="https://leefyorganics.com/" target="_blank" rel="noopener noreferrer">Leefy organics</a>
+          <a className="link-website" href="https://www.desmirage.com/" target="_blank" rel="noopener noreferrer">DesMirage</a>
+          <a className="link-website" href="https://www.casesbysource.com/" target="_blank" rel="noopener noreferrer">Cases By Source</a>
+          <a className="link-website" href="https://thepowerfit.com/" target="_blank" rel="noopener noreferrer">Power Fit </a>
+          <a className="link-website" href="https://runur.run/" target="_blank" rel="noopener noreferrer">Runer Run</a>
+          <a className="link-website" href="https://bddbrands.com/" target="_blank" rel="noopener noreferrer">BDD Brands </a>
+          <a className="link-website" href="https://www.colorlabtransfers.com/" target="_blank" rel="noopener noreferrer">ColorLab Transfer</a>
+          <a className="link-website" href="https://ipsopromo.com/pages/landing-page" target="_blank" rel="noopener noreferrer">Ipso Facto</a>
+          <a className="link-website" href="https://healthvape.com/" target="_blank" rel="noopener noreferrer">Health vape</a>
+
+          <a className="link-website" href="https://novatechgaming.com/" target="_blank" rel="noopener noreferrer">Nova Techgaming</a>
+          <a className="link-website" href="https://leefyorganics.com/" target="_blank" rel="noopener noreferrer">Leefy organics</a>
         </div>
       </div>
 
